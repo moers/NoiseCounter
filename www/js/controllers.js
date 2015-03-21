902f7a6ad42303ab01aa27a6607eb979a22541fa
Elmar Burke <elmar.burke@symetics.com>
2015-03-21 17:45:12 +0100

    full screen maps

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,9 +3,13 @@ angular.module('starter.controllers', [])
 .controller('MapCtrl', function($scope) {
   mapboxgl.accessToken = 'pk.eyJ1IjoiZWxtYXJidXJrZSIsImEiOiJmYlAzYl80In0.Odg6o8oVaZHGSjB5HY9G0A';
   // Create a map in the div #map
-  new mapboxgl.Map({
+  var map = new mapboxgl.Map({
     container: 'map',
     style: 'https://www.mapbox.com/mapbox-gl-styles/styles/outdoors-v7.json'
+  });
+
+  map.on('move', function(e) {
+    console.log('move', e);
   });
 })
 
